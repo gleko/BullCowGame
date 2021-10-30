d07f18d8e6d6c904baa4de732fd1cf148fdae418
Gergely LEKO-ROZSA <gergely@leko-rozsa.eu>
2021-10-30 16:01:31 +0200

    Print a welcome message

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("This is a Bull Cows Game!");
+    PrintLine("Let's try to find out the word...");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
