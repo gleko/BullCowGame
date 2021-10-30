465c361d53a2e9c5fc7d7e298c2c7ef6a1d5b678
Gergely LEKO-ROZSA <gergely@leko-rozsa.eu>
2021-10-30 21:01:31 +0200

    Add EndGame function

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,47 +5,71 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    PrintLine(TEXT("This is a Bull Cows Game!"));
-    PrintLine(TEXT("Guess the 6 letter word")); // TODO: Remove magic number
-    PrintLine(TEXT("Let's try to find out the word..."));
-
     SetupGame();
+
+    PrintLine(TEXT("[DEBUG]  The hidden word is: %s"), *HiddenWord);
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-
-    if (Input == HiddenWord)
+    if (bGameOver)
     {
-        PrintLine(TEXT("You have guessed correctly"));
-
-        // Play again?
-        // Yes: Reset game
-        // No: Quit game
+        ClearScreen();
+        SetupGame();
     }
     else
     {
-        PrintLine(TEXT("Your guess is incorrect"));
+        if (Input == HiddenWord)
+        {
+            PrintLine(TEXT("You have guessed correctly"));
+            EndGame();
+
+            // Play again?
+            // Yes: Reset game
+            // No: Quit game
+        }
+        else
+        {
+            if (HiddenWord.Len() != Input.Len())
+            {
+                PrintLine(TEXT("The length of the word is incorrect\nLength should be %i"), HiddenWord.Len());
+                EndGame();
+            }
         
-        // Subtract a life
+            // Subtract a life
 
-        // Are there remaining lives?
-        // No: You lose, print lost message, ask if play again
+            // Are there remaining lives?
+            // No: You lose, print lost message, ask if play again
 
-        // Is the word length correct?
-        // No: Remind the word length
+            // Is the word length correct?
+            // No: Remind the word length
 
-        // Is word an isogram?
-        // No: you need to give an isogram
+            // Is word an isogram?
+            // No: you need to give an isogram
 
-        // Calculate hints based on Input 
-        // and give feedback to player
+            // Calculate hints based on Input 
+            // and give feedback to player
+        }
     }
+
+    // if the game is over, then do something
+    // else check the player guess
+
 }
 
 void UBullCowCartridge::SetupGame()
 {
-    HiddenWord = TEXT("number");
+    HiddenWord = TEXT("cakes");
     Lives = 4;
+    bGameOver = false;
+
+    PrintLine(TEXT("This is a Bull Cows Game!"));
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len()); // TODO: Remove magic number
+    PrintLine(TEXT("Let's try to find out the word..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Game is over\nPress ENTER to play again..."));
 }
